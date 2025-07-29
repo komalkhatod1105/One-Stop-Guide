d7e13a3745d4edb653c7e5c3f08e8e0e39092f45
Komal Khatod <komalkhatod1234@gmail.com>
2025-07-29 19:47:28 +0530

    🔧 Updated: Modified features and UI changes

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,11 @@
 <?php
 $host = "localhost";
 $user = "root";
-$password = "230184"; // Default XAMPP password is blank
+$password = "Tarunkha@2007"; // Default XAMPP password is blank
 $database = "CityGuideDB";
 
 // Establish connection
 $conn = mysqli_connect($host, $user, $password, $database);
-
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
